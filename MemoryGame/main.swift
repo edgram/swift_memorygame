c9f6380a2873a62aa5b9b6dee0fa8510800dabef
Edgar Ramirez <edgar@macbook-pro-de-edgar.local>
2015-12-15 22:31:07 -0600

    Initial Commit

---
+++ MemoryGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MemoryGame
+//
+//  Created by Edgar Ramirez on 15/12/15.
+//  Copyright © 2015 Edgar Ramirez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
